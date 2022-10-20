f535bff361a09680509760d052c36ab62f3dab00
willfranck <jspec520@gmail.com>
2022-10-20 16:14:22 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# okava-coffee-design
+ Testing a design for a local coffee shop
---
+++ src/script.js
@@ -0,0 +1,50 @@
+//********** Search Bar **********/
+
+const searchBar = document.getElementById('search-bar');
+const searchInput = document.getElementById('nav-search-input');
+const searchButton = document.getElementById('nav-search-button');
+
+searchBar.addEventListener('mouseover', searchBarExpand);
+searchInput.addEventListener('focusin', searchBarExpand);
+searchButton.addEventListener('click', searchBarExpand);
+
+searchBar.addEventListener('mouseleave', searchBarCollapse);
+searchInput.addEventListener('focusout', searchBarCollapse);
+
+function searchBarExpand() {
+    searchInput.classList.add('input-active');
+    searchButton.classList.add('button-active');
+}
+
+function searchBarCollapse() {
+    if(searchInput != document.activeElement) {
+        searchInput.classList.remove('input-active');
+        searchButton.classList.remove('button-active');
+    }
+}
+
+searchButton.onclick = function() {
+    searchInput.focus();
+}
+
+
+//*********** Menu Bar ************/
+
+const menuButton = document.getElementById('menu-button');
+const menuSlider = document.getElementById('side-nav-links');
+const menuCloseButton = document.getElementById('menu-close-button');
+
+menuButton.addEventListener('click', menuBarExpand);
+menuCloseButton.addEventListener('click', menuBarCollapse);
+
+function menuBarExpand() {
+    menuSlider.classList.add('side-nav-links-active');
+    menuButton.style.display = 'none';
+    menuCloseButton.style.display = 'block';
+}
+
+function menuBarCollapse() {
+    menuSlider.classList.remove('side-nav-links-active');
+    menuButton.style.display = 'block';
+    menuCloseButton.style.display = 'none';
+}
